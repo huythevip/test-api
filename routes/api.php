5ec728b40f3c9e2cc33ee8ca99fd7fb04c53f6f2
huythevip <huy.tq2106@gmail.com>
2018-04-13 18:28:04 +0700

    Change route name

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::get('users', 'UserController@index');
-Route::post('users', 'UserController@create');
+Route::post('users', 'UserController@store');
 Route::get('users/{id}', 'UserController@show');
 Route::put('users/{id}', 'UserController@update');
 Route::delete('users/{id}', 'UserController@delete');
