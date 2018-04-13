32d71939564c6d72c3ccde74e1683551ba087a0a
huythevip <huy.tq2106@gmail.com>
2018-04-13 09:58:28 +0700

    First Commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('gender');
+            $table->integer('timezone', 2);
+            $table->date('birthday');
+            $table->string('location');
+            $table->boolean('had_feedback_email');
+            $table->boolean('sync_name_bio');
+            $table->text('bio');
+            $table->string('picture_url');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
